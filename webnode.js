e4e1c56dff546c8b0aa1f0b269c7ef3e17b3acad
Edward Bristow <11273941+ed-bristow@users.noreply.github.com>
2018-09-19 21:56:22 +0100

    new socket.io

--- webnode.js
+++ webnode.js
@@ -1,13 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
     fs.readFile('website.html', function(err, data) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(data);
 		return res.end();
 	});
-}).listen(80);
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function (socket) {
+    console.log('Client connected!');
+});
+
+server.listen(80);
 
 // cd C:\Users\Edward\Documents\Programming\Notepad++ Files\text saving website\
 // node webnode.js
